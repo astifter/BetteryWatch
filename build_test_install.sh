f76ddef6b8a69fae33d3f3812b9178709dd3e3b1
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-05-11 23:10:33 +0200

    Added color background.

---
+++ build_test_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+start_emulator=false
+
+pebble build
+if [[ -n "$1" ]]; then
+    if [[ "$1" == "aplite" || "$1" == "basalt" ]]; then
+        target="--emulator $1"
+    else
+        target="--phone $1"
+        echo -n "Press [Enter] to install, [Ctrl-C] to exit."; read -n 1 -s;
+    fi
+        
+    pebble install $target
+    pebble logs $target
+fi
